acbbf77d15cf0be241de607c790d5a292f78b673
Marcel Suter <marcel.suter@bzz.ch>
2024-02-03 08:07:29 +0100

    Update grain.py

--- grain.py
+++ grain.py
@@ -1,3 +1,6 @@
 class Grain:
     def __str__(self):
+        """
+        string representation of a grain
+        """
         return f"Grain(name='{self.name}', price={self.price}, allergen='{self.allergen}')"
