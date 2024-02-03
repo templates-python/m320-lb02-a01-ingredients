eb767efacdcf695c9d9cfcc045026f835c9faad6
Marcel Suter <marcel.suter@bzz.ch>
2024-02-03 08:06:53 +0100

    Update additive.py

--- additive.py
+++ additive.py
@@ -1,3 +1,6 @@
 class Additive:
     def __str__(self):
+        """
+        string representation of an addtive
+        """
         return f"Additive(name='{self.name}', price={self.price}, allergen='{self.allergen}', origin='{self.origin}')"
