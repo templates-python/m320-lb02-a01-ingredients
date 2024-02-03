1b81cd727da89a3da0c0eae4993c52711249eedb
Marcel Suter <marcel.suter@bzz.ch>
2024-02-03 08:07:10 +0100

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -1,3 +1,6 @@
 class Fruit:
     def __str__(self):
+        """
+        string representation of a fruit
+        """
         return f"Fruit(name='{self.name}', price={self.price}, origin='{self.origin}')"
